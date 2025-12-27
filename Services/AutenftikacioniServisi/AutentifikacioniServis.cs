97815c534cf3131578f79c4c970afa539ec47506
IgnjatRadojicic <ignjatradojicic@gmail.com>
2025-12-27 11:00:36 +0100

    feat: Added User Authentication

--- Services/AutenftikacioniServisi/AutentifikacioniServis.cs
+++ Services/AutenftikacioniServisi/AutentifikacioniServis.cs
@@ -1,16 +1,45 @@
-﻿using Domain.Modeli;
-using Domain.Servisi;
+﻿using Domain.Interfaci;
+using Domain.Modeli;
+using Domain.Modeli.Enumeracije;
+using Domain.Repozitorijumi;
 
 namespace Services.AutenftikacioniServisi
 {
     public class AutentifikacioniServis : IAutentifikacijaServis
     {
-        // TODO: Add necessary dependencies (e.g., user repository) via dependency injection
+        private readonly IKorisniciRepozitorijum korisniciRepozitorijum;
+        private readonly ILoggerServis loggerServis;
 
-        public (bool, Korisnik) Prijava(string korisnickoIme, string lozinka)
+        public AutentifikacioniServis(IKorisniciRepozitorijum korisniciRepozitorijum, ILoggerServis loggerServis)
         {
-            // TODO: Implement login method
-            throw new NotImplementedException();
+            this.korisniciRepozitorijum = korisniciRepozitorijum;
+            this.loggerServis = loggerServis;
+        }
+
+        public (bool, Korisnik) Prijava(string korisnickoIme, string loznika)
+        {
+            try
+            {
+                Korisnik korisnik = korisniciRepozitorijum.PronadjiKorisnikaPoKorisnickomImenu(korisnickoIme);
+
+                if(korisnik.KorisnickoIme == string.Empty)
+                {
+                    loggerServis.EvidentirajDogadjaj(TipEvidencije.WARNING, $"Neuspešan pokušaj prijave: korisnik '{korisnickoIme}' ne postoji");
+                    return (false, new Korisnik());
+                }
+
+                if(korisnik.Lozinka != loznika)
+                {
+                    loggerServis.EvidentirajDogadjaj(TipEvidencije.WARNING, $"Neuspešan pokušaj prijave: pogrešna lozinka za korisnika '{korisnickoIme}'");
+                }
+                loggerServis.EvidentirajDogadjaj(TipEvidencije.INFO, $"Uspešna prijava korisnika '{korisnickoIme}' ({korisnik.Uloga})");
+                return (true, korisnik);
+            }
+            catch (Exception ex)
+            {
+                loggerServis.EvidentirajDogadjaj(TipEvidencije.ERROR, $"Greška pri prijavi: {ex.Message}");
+                return (false, new Korisnik());
+            }
         }
     }
 }
